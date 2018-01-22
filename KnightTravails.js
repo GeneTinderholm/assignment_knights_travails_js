32dbce45d92ec49e26c15fd682fa40dede5c2643
Jeff Dederick <Xpressivmotion@gmail.com>
2018-01-22 18:46:09 -0500

    debugging boards

--- KnightTravails.js
+++ KnightTravails.js
@@ -11,57 +11,65 @@ class Move {
     this.depth = depth;
     this.moves = ['lu', 'ru', 'ld', 'rd', 'ul', 'dl', 'ur', 'dr'];
     this.actions = {
-      lu: {x: this.start.x - 2, y: this.start.y - 1},
-      ru: {x: this.start.x + 2, y: this.start.y - 1},
-      ld: {x: this.start.x - 2, y: this.start.y + 1},
-      rd: {x: this.start.x + 2, y: this.start.y + 1},
-      ul: {x: this.start.x - 1, y: this.start.y - 2},
-      dl: {x: this.start.x - 1, y: this.start.y + 2},
-      ur: {x: this.start.x + 1, y: this.start.y - 2},
-      dr: {x: this.start.x + 1, y: this.start.y + 2},
+      lu: { x: this.start.x - 2, y: this.start.y - 1 },
+      ru: { x: this.start.x + 2, y: this.start.y - 1 },
+      ld: { x: this.start.x - 2, y: this.start.y + 1 },
+      rd: { x: this.start.x + 2, y: this.start.y + 1 },
+      ul: { x: this.start.x - 1, y: this.start.y - 2 },
+      dl: { x: this.start.x - 1, y: this.start.y + 2 },
+      ur: { x: this.start.x + 1, y: this.start.y - 2 },
+      dr: { x: this.start.x + 1, y: this.start.y + 2 }
     };
   }
   nextMove() {
     if (this.depth === 63) {
-      console.log('ding');
-      return {result: true, stack: [this.start]};
+      // console.log('ding');
+      return { result: true, stack: [this.start] };
     }
     let result = false;
     for (let i = 0; i < 8; i++) {
+      let nextBoard = this.board
+        .slice(0)
+        .map(subArray => subArray.slice(0).map(el => el));
       let moveAbrv = this.moves.pop();
       let nextPosition = this.actions[moveAbrv];
       if (
-        between(nextPosition.x, 0, 7) &&
         between(nextPosition.y, 0, 7) &&
-        this.board[nextPosition.y][nextPosition.x]
+        between(nextPosition.x, 0, 7) &&
+        this.board[nextPosition.x][nextPosition.y]
       ) {
-        let nextBoard = this.board;
-        nextBoard[nextPosition.y][nextPosition.x] = 0;
-        console.log(
-          'depth: ',
-          this.depth,
-          '\nboard: \n',
-          this.board,
-          '\n start:',
-          this.start,
-          '\nmoves:',
-          this.moves,
-        );
-        result = new Move(nextPosition, nextBoard, this.depth + 1).nextMove();
+        nextBoard[nextPosition.x][nextPosition.y] = 0;
+        // console.log(
+        //   'depth: ',
+        //   this.depth,
+        //   '\nboard: \n',
+        //   this.board,
+        //   '\n start:',
+        //   this.start,
+        //   '\nmoves:',
+        //   this.moves
+        // );
+        let resultMove = new Move(nextPosition, nextBoard, this.depth + 1);
+        result = resultMove.nextMove();
         if (result) {
-          console.log('result: ', result, '\ndepth: ', this.depth);
+          // console.log('result: ', result, '\ndepth: ', this.depth);
+          // console.log('Pandas!!!!!!!!+++++++++===========================');
           result.stack.push(this.start);
           break;
         }
       } else {
-        console.log(nextPosition, '\n', this.board);
+        // console.log(nextPosition, 'm\n ', this.board);
+        // nextBoard = this.board;
+        // console.log('FAIL!!!!!!!!!!!!!!');
         continue;
       }
     }
     if (this.moves.length === 0) {
       console.log('out of moves');
+      // nextBoard = this.board;
       return false;
     }
+    console.log(`Results:======================= ${result}`);
     return result;
   }
 }
@@ -76,13 +84,13 @@ class Knight {
       [1, 1, 1, 1, 1, 1, 1, 1],
       [1, 1, 1, 1, 1, 1, 1, 1],
       [1, 1, 1, 1, 1, 1, 1, 1],
-      [1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 1, 1, 1]
     ];
     // this.moveStack = [];
   }
-  gallop(y, x) {
-    this.board[y][x] = 0;
-    let result = new Move({x, y}, this.board, 0);
+  gallop(x, y) {
+    this.board[x][y] = 0;
+    let result = new Move({ x, y }, this.board, 0);
     console.log(result);
     return result.nextMove();
   }
